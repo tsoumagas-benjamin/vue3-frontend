d19d134712752971a8f7f51be42d06596bab4758
tsoumagas-benjamin <bptsoumagas@gmail.com>
2024-04-10 14:58:07 -0400

    Testing out sending prop values

---
+++ test/src/components/TestPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="hello">
+      <h1>{{ activity }}</h1>
+      <h1>{{ type }}</h1>
+      <h1>{{ participants }}</h1>
+      <h1>{{ price }}</h1>
+      <h1>{{ link }}</h1>
+      <h1>{{ keyVal }}</h1>
+      <h1>{{ accessibility }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TestPage',
+    props: {
+      activity: String,
+      type: String,
+      participants: Number,
+      price: Number,
+      link: String,
+      keyVal: String,
+      accessibility: Number
+    }
+  }
+  </script>
+  
---
+++ test/src/views/TestView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Test">
+      <h1>This is a test page</h1>
+      <TestPage msg="Welcome to Your Vue.js App"/>
+    </div>
+  </template>
+  
+<script>
+// @ is an alias to /src
+import TestPage from '@/components/TestPage.vue'
+
+export default {
+  name: 'TestView',
+  components: {
+    TestPage
+  }
+}
+</script>
